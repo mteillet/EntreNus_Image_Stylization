ffb82397d40aabbbed9aa2d6cd414b4f110ce0ee
Martin Teillet <54497763+mteillet@users.noreply.github.com>
2019-11-22 20:52:06 +0100

    Generation_of_brushes_Done

--- functions/draw_noise/noiseRecognition.py
+++ functions/draw_noise/noiseRecognition.py
@@ -18,21 +18,42 @@ def main():
     loc = np.where(res >= threshold)
     
     # Drawing Rectangles on the locators where the the pattern has been detected
-    size = 10
+    size = 70
     current = 0
-    for i in loc[1]:        
+    roiXstart = []
+    roiYstart = []
+    roiXend = []
+    roiYend = []
+    for i in loc[1]:
+        # Start and end positions
         startY = (loc[0][current]) - size
         startX = (loc[1][current]) - size
         endY = (startY) + (size * 2)
         endX = (startX) + (size * 2)
-        cv2.rectangle(imgGrid, (startX, startY),(endX, endY), (1.0,0.0,0.0), 1)
+        # Drawing Image - Rectangle for debugging purposes
+        #cv2.rectangle(imgGrid, (startX, startY),(endX, endY), (1.0,0.0,0.0), 1)
+        roiXstart.append(startX)
+        roiYstart.append(startY)
+        roiXend.append(endX)
+        roiYend.append(endY)
+
+        # Defining ROI as an image
+        roiMain = imgGrid[startY:endY,startX:endX]
+        # Resize imgBrush to ROI
+        roiBrush = cv2.resize(imgBrush,(roiMain.shape[1], roiMain.shape[0]))
+
+        # Adds the last roiBrush on top of the roiMain in order to have overlapping brush strokes 
+        roi_Computed = cv2.add(roiMain, roiBrush)
+
+        # Place the ROI on a new image size of the grid frame
+        imgGrid[startY:endY,startX:endX] = roi_Computed
+        
+
         current += 1
         
-    #other_image = (255*np.random.rand(*imgGrid.shape)).astype(np.uint8)
-    #imgBrush.copyTo(imgGrid(cv2.rectangle500,500,imgBrush.cols, imgBrush.rows))
+        
 
 
-    
     cv2.imshow('image', imgGrid)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
